b8433def4ccf450bb55485ba04825e5b3e5fd9bc
Arun2133 <arunjaya49@gmail.com>
2021-09-15 11:55:00 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+ 
+
+function App() {
+  return (
+    <div className="App">
+       I AM A SOFTWARE ENGINEER
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+{
+  var a=5;
+  var b=10;
+  var c=a+b;
+  document.getElementById('root')
+}
+a();
+ 
+
+ 
